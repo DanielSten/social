7b91025fc8180c9f6faa8a689167fcb74177a6f4
sertamon <stenyaev1997@gmail.com>
2022-07-15 23:10:09 +0300

    Добавление окон авторизации и регистрации

--- js/script.js
+++ js/script.js
@@ -1,16 +1,28 @@
 const buttonRegistration =  document.querySelector(".block_button_registarion");
 const buttonAutorization = document.querySelector(".block_button_autorization");
 
+
 if (buttonRegistration) {
     const openRegistration = document.querySelector(".block_emergence_registration");
+    const closeRegistration = document.getElementById("closeRegistarion");
     buttonRegistration.addEventListener("click", function() {
         openRegistration.classList.add("active");
+    })
+    closeRegistration.addEventListener("click", function() {
+        openRegistration.classList.toggle("active");
     })
 }
 
 if (buttonAutorization) {
     const openAutorization = document.querySelector(".block_emergence_autorization");
+    const closeAutorization = document.getElementById("closeAutorization");
     buttonAutorization.addEventListener("click", function() {
-        openAutorization.classList.add("active");
+        openAutorization.classList.toggle("active");
     })
+    closeAutorization.addEventListener("click", function() {
+        openAutorization.classList.toggle("active");
+    })
+
 }
+
+
