4d04fbdb4446b22c8a777d072159b730dd7813df
sertamon <stenyaev1997@gmail.com>
2022-07-19 22:44:39 +0300

    Добавлена вторая страница с лентой

--- js/script.js
+++ js/script.js
@@ -1,5 +1,7 @@
 const buttonRegistration =  document.querySelector(".block_button_registarion");
 const buttonAuthorization = document.querySelector(".block_button_authorization");
+const buttonAdded = document.getElementById('buttonAdded');
+const buttonDelete = document.getElementById('buttonDelete');
 
 
 if (buttonRegistration) {
@@ -23,6 +25,34 @@ if (buttonAuthorization) {
         openAuthorization.classList.toggle("active");
     })
 
+}
+
+if (uploadFile) {
+    uploadFile.addEventListener('change', function(){
+        if(this.value) {
+            buttonAdded.classList.toggle('hidden');
+            buttonDelete.classList.toggle('hidden');
+        }
+    });
+}
+
+buttonDelete.onclick = function() {
+    if (uploadFile.value) {
+        uploadFile.value=''
+        buttonAdded.classList.toggle('hidden');
+        buttonDelete.classList.toggle('hidden');
+        if(buttonAdded.classList.contains('hidden') == false ){
+            document.getElementById('conclusionFile').innerText = '';
+        }
+    }
+}
+
+let showFile = function (input) {
+    let file = input.files[0];
+    let fileName = `${file.name}`;
+    if(buttonAdded.classList.contains('hidden') == false){
+        document.getElementById('conclusionFile').innerText = fileName;
+    }
 }
 
 
